c99a102c9ab208211de2bb50aa87ca9edd2b4a9c
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-11-01 17:57:44 +0100

    ShoppingListAppAddToGithub

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import ShoppingLists from './components/ShoppingLists'; // import the screens
+import Welcome from './components/Welcome';
 
-export default function App() {
+// Create the stack navigator
+const Stack = createNativeStackNavigator();
+
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBU5sMsZVlLU182rQREXRTMsrpb8e21RH0",
+    authDomain: "shopping-list-demo-301d9.firebaseapp.com",
+    projectId: "shopping-list-demo-301d9",
+    storageBucket: "shopping-list-demo-301d9.firebasestorage.app",
+    messagingSenderId: "483198929970",
+    appId: "1:483198929970:web:8d3fdca6e07fd586e0c02b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen
+          name="ShoppingLists"
+        >
+          {props => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
